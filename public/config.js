c253121b73988423c4a720ff830ee8da8ca8d061
orderly-guoqing <guoqing@orderly.network>
2026-08-13 21:15:04 +0800

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -22,7 +22,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_ENABLED_MENUS": "Trading,Markets,Vaults,Portfolio",
   "VITE_CUSTOM_MENUS": "",
   "VITE_ENABLE_SERVICE_DISCLAIMER_DIALOG": "false",
-  "VITE_ENABLE_CAMPAIGNS": "true",
+  "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/secureshiftTg",
   "VITE_DISCORD_URL": "",
   "VITE_TWITTER_URL": "https://x.com/secureshift_io",
